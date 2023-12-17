93a5806d82e7d031f29399a6ba9c7d0627f02beb
MSP079 <m.saipraneeth23@gmail.com>
2023-12-17 22:47:09 +0530

    Final Stage completed

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { Route, Navigate , Routes } from "react-router-dom";
+import AppBar from "./AppBar";
+import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./common/ProtectedRoute";
+import AddProductForm from "./AddProduct";
+import EditProductForm from "./EditProduct";
+import CreateOrder from "./CreateOrder";
+
+
+function App() {
+
+  return (
+    <>
+      <ToastContainer />
+      <AppBar />
+      <Routes>
+        <Route path="./login" component={Login} />
+        <Route path="./signup" component={SignUp} />
+        <ProtectedRoute adminOnly={true} path="/add-product" component={AddProductForm}  />
+        <ProtectedRoute adminOnly={true} path="/modify-product/:id" component={EditProductForm}  />
+        <ProtectedRoute adminOnly={false} path="/order/:id/:quantity" component={CreateOrder}  />
+        <ProtectedRoute adminOnly={false} path="/" component={Home} />
+        <Navigate  to="./login" />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
